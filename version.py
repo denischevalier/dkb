5dc73cb9ac8c830ce7c5214d270938c3d5677e16
Denis Chevalier <chevalierdenis@gmx.com>
2014-09-14 03:28:20 +0200

    Added some files from the original pykeylogger used as includes in dkb

---
+++ version.py
@@ -0,0 +1,13 @@
+# Change this for stealth. you must also change the filenames of the
+# .ini, .val, and the icon files and replace "pykeylogger" with what you put in
+# here.
+name = "dkb"
+# Change this for stealth.
+description = "Simple Python Keybinder for linux (Xorg)"
+
+version = "0.1"
+url = "https://github.com/denischevalier/dkb/"
+license = "GPLv3"
+author = "Denis Chevalier"
+author_email = "chevalierdenis@gmx.com"
+platform = "Linux"
