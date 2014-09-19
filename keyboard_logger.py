fbdcd9ef6b6ba784c5e47b5e3609a05d6b3bd69d
Denis Chevalier <chevalierdenis@gmx.com>
2014-09-20 00:46:57 +0200

    Adding comments in keyboard_logger.py And finishing reducing foss

--- keyboard_logger.py
+++ keyboard_logger.py
@@ -34,40 +34,39 @@ import signal
 
 class KeyboardLogger:
     def __init__(self):
-        self.hm = pyxhook.HookManager()
-        self.hm.HookKeyboard()
-        self.hm.KeyDown = self.OnKeyDownEvent
+        self.hm = pyxhook.HookManager()                             # Initialize HookManager
+        self.hm.HookKeyboard()                                      # We're only interested in the keyboard events
+        self.hm.KeyDown = self.OnKeyDownEvent                       # Bind self.OnKeyDownEvent() to hm.KeyDown
 
     def start(self):
-        print(('[INFO]Starting Logger thread'), file=sys.stderr)
-        self.hm.start()
+        self.hm.start()                                              # Starting the HookManager
 
     def OnKeyDownEvent(self, event):
-        if str(event.Key) == 'Scroll_Lock':
-            print('Scroll_Lock')                    # Permit keylog_parser.py to quit properly
-            self.stop()
-            return
-        self.eventkey = event.Key + '\n'
-        p = Process(target=self.write_keycode)
-        p.start()
-        p.join()
+        if str(event.Key) == 'Scroll_Lock':                         # If we intercept the Scroll_Lock key (exit key)
+            print('Scroll_Lock')                                    # Permit keylog_parser.py to quit properly
+            self.hm.cancel()                                        # Stop the HookManager
+            time.sleep(0.2)                                         # Wait for all threads to finish properly
+            sys.exit(0)                                             # Exit with EXIT_SUCCESS
+
+        self.keycode = event.Key                                    # Register the keycode internally
+        p = Process(target=self.write_keycode)                      # Write the keycode asynchronously 
+                                                                    # (to permit keylog_parser.py to accessit)
+        p.start()                                                   # Start the process
+        p.join()                                                    # Join the process with the current thread
 
     def write_keycode(self):
-        sys.stdout.write(self.eventkey)
-
-    def stop(self):
-        self.hm.cancel()
-        time.sleep(0.2)
-        sys.exit()
+        sys.stdout.write(self.keycode + '\n')                       # Write the keycode to stdout
 
 def SigIntHandler(signum, frame):
-    print ('[WARNING]SIGINT (Ctrl+C) signal received, continuing:Please exit keyboard_logger.py properly by pressing the Scroll_Lock keyboard key.',
-            file=sys.stderr)
+    print ('[WARNING]SIGINT (Ctrl+C) signal received, continuing:'
+            'Please exit keyboard_logger.py properly by pressing '
+            'the Scroll_Lock keyboard key.',
+            file=sys.stderr)                                        # Disabling SIGINT to permit threads to finish
+                                                                    # Properly by pressing Scroll_Lock
 
 if __name__ == '__main__':
-    # Set the signal handler for SIGINT
-    signal.signal(signal.SIGINT, SigIntHandler)
-    kl = KeyboardLogger()
-    kl.start()
+    signal.signal(signal.SIGINT, SigIntHandler)                     # Setting the signal handler for SIGINT
+    kl = KeyboardLogger()                                           # Instanciating KeyboardLogger
+    kl.start()                                                      # Starting the hook manager
 
 
