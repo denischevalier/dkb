49e8e8d9192b3713ca6a1060e23d8fde6a4edf3f
Denis Chevalier <chevalierdenis@gmx.com>
2014-09-21 22:07:30 +0200

    OOP config_parser and creation of 2 methods:
    ConfigParser.verif_config(), that performs different tests on the json
    imported from config file.
    
    ConfigParser.get_config_action(), that checks if an action is triggered
    for the existing buffer and returns its string if it is.

--- config_parser.py
+++ config_parser.py
@@ -24,9 +24,51 @@
 ##########################################################################
 
 import json
+import sys
 
-def ConfigParser(config_file):
-    whith open(config_file, "rt") as in_file:           # Open the config file in read only mode 
-        text = in_file.read()                           # Put its content in memory
-    config = json.loads(text)                           # Parse json config
-    return config                                       # Return the resulting list
+class ConfigParser():
+    def __init__(self, config_file):
+        whith open(config_file, "rt") as in_file:                                           # Open the config file in read only mode 
+            text = in_file.read()                                                           # Put its content in memory
+        self.config = json.loads(text)                                                      # Parse json config
+        self.verif_config()                                                                 # Check the parsed data for errors
+
+    def verif_config(self):
+        print ('[DEBUG]Checking config file integrity',file=sys.stderr)
+
+        # Test the root config object
+        if len(self.config) == 0:                                                           # If the object is empty, return quitely
+            return
+        if type(self.config) != "<class 'dict'>":                                           # The result object must be of type 'dict'
+            raise Exception("The root type object is " + type(self.config) + 
+                    " instead of <class 'dict'>")
+
+        # Test the sub objects
+        for val in self.config:                                                             # Loop over the config object
+            if type(val) != "<class 'list'>":                                               # The elements of the config oject must be
+                                                                                            # of type 'list'
+                raise Exception('The elements of the config object must be lists.\n'
+                        'They actually are ' + type(val))
+            if len(val) < 1 and len(val) > 4:                                               # Each element must have a maximum of 4 members
+                raise Exception('The lists in config obj can\'t be longer than 4 members.')
+            for elt in val:                                                                 # Loop over subelements
+                if type(elt) != "<class 'str'>":                                            # They must be strings
+                    raise Exception('Each sublist member must be of type <class \'str\'>.\n'
+                            'Found type ' + type(elt) + ' instead.')
+
+    def get_config_action(self, buffer):
+        for action in self.config:                                                          # Loop over the config object
+            if len(self.config[action]) == 1:                                               # A keybind can be of either 1, 2, 3 or 4 keys
+                if buffer[4] == self.config[action]:                                        # Return action if the actual buffer matches the
+                    return action                                                           # config.
+            if len(self.config[action]) == 2:
+                if buffer[3:4] == self.config[action]:
+                    return action
+            if len(self.config[action]) == 3:
+                if buffer[2:4] == self.config[action]:
+                    return action
+            if len(self.config[action]) == 4:
+                if buffer == self.config[action]:
+                    return action
+        return None                                                                         # Else return None
+
