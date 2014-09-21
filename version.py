fc25d2f12200c9eb8454b494f5f127a00bf9fa5b
Denis Chevalier <chevalierdenis@gmx.com>
2014-09-21 23:01:10 +0200

    Modifying version.py to remove non pertinent comments

--- version.py
+++ version.py
@@ -1,10 +1,5 @@
-# Change this for stealth. you must also change the filenames of the
-# .ini, .val, and the icon files and replace "pykeylogger" with what you put in
-# here.
 name = "dkb"
-# Change this for stealth.
 description = "Simple Python Keybinder for linux (Xorg)"
-
 version = "git"
 url = "https://github.com/denischevalier/dkb/"
 license = "GPLv3"
