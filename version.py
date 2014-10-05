35e5735aad478100ddc8371258841044a62fbbd7
Denis Chevalier <chevalierdenis@gmx.com>
2014-10-05 21:15:49 +0200

    Update version.py

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 name = "dkb"
 description = "Simple Python Keybinder for linux (Xorg)"
-version = "0.2.1"
+version = "0.2.2"
 url = "https://github.com/denischevalier/dkb/"
 license = "GPLv3"
 author = "Denis Chevalier"
